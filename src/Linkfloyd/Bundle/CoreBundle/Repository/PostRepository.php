963137f1038d0fda85fc7129014bcbf8ebea57bc
shibby <guvenatbakan@gmail.com>
2016-11-20 01:03:18 +0300

    qury optimization

--- src/Linkfloyd/Bundle/CoreBundle/Repository/PostRepository.php
+++ src/Linkfloyd/Bundle/CoreBundle/Repository/PostRepository.php
@@ -2,7 +2,6 @@
 /**
  * @author Guven Atbakan <guven@atbakan.com>
  */
-
 namespace Linkfloyd\Bundle\CoreBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
@@ -12,8 +11,9 @@ class PostRepository extends EntityRepository
     public function findPosts()
     {
         return $this->createQueryBuilder('post')
-            ->select('post', 'linkDetail', 'user', 'detail')
+            ->select('post', 'linkDetail', 'user', 'detail', 'thumbnailMedia')
             ->join('post.linkDetail', 'linkDetail')
+            ->leftJoin('linkDetail.thumbnailMedia', 'thumbnailMedia')
             ->join('post.user', 'user')
             ->join('post.detail', 'detail')
             ->orderBy('post.id', 'DESC')
