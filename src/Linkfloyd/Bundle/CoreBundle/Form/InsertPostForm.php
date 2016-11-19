c22280c20b5ea6180962679fb4d97fab3b615b08
shibby <guvenatbakan@gmail.com>
2016-11-19 16:03:19 +0300

    validations added for insert url form

--- src/Linkfloyd/Bundle/CoreBundle/Form/InsertPostForm.php
+++ src/Linkfloyd/Bundle/CoreBundle/Form/InsertPostForm.php
@@ -9,6 +9,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Url;
 
 class InsertPostForm extends AbstractType
 {
@@ -17,10 +20,21 @@ class InsertPostForm extends AbstractType
         $builder
             ->add('url', UrlType::class, [
                 'label_format' => 'form.insert_post.labels.url',
+                'constraints' => [
+                    new NotBlank(['message' => 'form.insert_post.validation.url_not_blank']),
+                    new Url(['message' => 'form.insert_post.validation.url_valid_url']),
+                ],
             ])
             ->add('title', TextareaType::class, [
                 'label_format' => 'form.insert_post.labels.title',
                 'attr' => ['rows' => 3, 'maxlength' => 350],
+                'constraints' => [
+                    new NotBlank(['message' => 'form.insert_post.validation.title_not_blank']),
+                    new Assert\Length([
+                        'max' => 350,
+                        'maxMessage' => 'form.insert_post.validation.title_too_long',
+                    ]),
+                ],
             ])
             ->add('submit', SubmitType::class)
         ;
