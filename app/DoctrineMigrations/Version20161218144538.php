ba8dd185ca6054dd65ce95128468f0eb611d3642
guvenatbakan <guven@atbakan.com>
2016-12-18 15:47:13 +0300

    comment_count added to Post entity. vote disabled on comments.

---
+++ app/DoctrineMigrations/Version20161218144538.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161218144538 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE posts ADD comment_count INT UNSIGNED NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE posts DROP comment_count');
+    }
+}
