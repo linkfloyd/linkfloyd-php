b15db0c3c935726bc66fc352222e33ff0abf3bad
shibby <guvenatbakan@gmail.com>
2017-01-20 03:44:27 +0300

    floydstyle added to bower dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,17 @@ elixir(function (mix) {
 
     mix.scripts([
         '/jquery/dist/jquery.min.js',
-        '/bootstrap/dist/js/bootstrap.min.js',
+        //'/bootstrap/dist/js/bootstrap.min.js',
     ], 'web/assets/js/scripts.js', 'bower_components');
 
     mix.styles([
-        '/bootstrap/dist/css/bootstrap.min.css',
+        //'/bootstrap/dist/css/bootstrap.min.css',
+        '/FloydStyle/dist/css/floydstyle.css',
     ], 'web/assets/css/stylesheets.css', 'bower_components');
 
     mix.styles([
         'custom.css',
     ], 'web/assets/css/linkfloyd.css');
     mix.copy('bower_components/bootstrap/dist/fonts', 'web/assets/fonts');
+    mix.copy('bower_components/FloydStyle/example/images/logo.png', 'web/assets/logo.png');
 });
